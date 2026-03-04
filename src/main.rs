24362f35def40dd2125b73a2dffa114cdd61b0cb
lalitupadhyay-dev <ulalit498@gmail.com>
2026-03-04 22:23:49 +0530

    Learnt basic foundations of Rust

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // Integer
+    let x: i32 = 5;
+    let y: i32 = 5;
+
+    println!("x * y: {}", x * y);
+
+    // Boolean -----------------------------------------------------------------------------
+    let mut flag: bool = true;
+    println!("Flag: {}", flag);
+
+    flag = false;
+
+    println!("Flag: {}", flag);
+
+    // String ------------------------------------------------------------------------------
+    let str: String = String::from("Learning Rust!");
+
+    println!("String: {}", str);
+
+    // Accessing character at an index in string
+    let ch: Option<char> = str.chars().nth(100);
+
+    // Checking character is abailable at "nth" index safely by pattern matching
+    match ch {
+        Some(c) => println!("{}", c),
+        None => println!("No character is found!"),
+    }
+
+}
